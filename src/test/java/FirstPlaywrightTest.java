d2b36a7d87ea236e4b6674a82fcdc12e5931c4f6
SamuelPutraPersonal <samueldianpermanaputra@gmail.com>
2025-04-30 11:07:40 +0200

    1. Commit the POM and first playwright class

--- src/test/java/FirstPlaywrightTest.java
+++ src/test/java/FirstPlaywrightTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import com.microsoft.playwright.*;
+
 
 public class FirstPlaywrightTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            System.out.println("Title of the page: " + page.title());
+
+            // wait for username input to be visible
+            
+
+
+            browser.close();
+        }
+    }
 }
